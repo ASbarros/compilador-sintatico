2e1c220d4cbd33ecc56a829b2f0e918100c00788
asbarros <anderson.barros0298@gmail.com>
2020-04-08 16:44:21 -0300

    refatorando

--- tipos/fld.js
+++ tipos/fld.js
@@ -1,7 +1,12 @@
 // para verificar se eh campo
+const reserv = require('./reserv.js')
+const comp = require('./comp.js')
+const constante = require('./const.js')
+
 const isFld = (arg) => {
     if (arg == '*') return 'TODOS'
-    if (arg.toString().match(/[r-z]/)) return false
+    if (reserv.isReserv(arg) || comp.isComp(arg) || constante.isConst(arg)) return false
+    if (arg.match(/[r-z]/g)) return false
     return true;
 }
 
